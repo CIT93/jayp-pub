14542cf7deba7c9867e15e7f81a78c09c8a58e8c
Jay C. Parangalan <stellarfirefly@yahoo.com>
2024-03-02 11:36:08 -0800

    transferred from private repo

---
+++ w9/footprint.js
@@ -0,0 +1,37 @@
+function determineHouseholdPts(inputText){
+    const numberInHousehold = parseInt(inputText);
+    if(numberInHousehold === 1){            // user lives alone
+        return 14;
+    } else if(numberInHousehold === 2){     // user lives with 1 other person
+        return 12;
+    } else if(numberInHousehold === 3){     // user lives with 2 other people
+        return 10;
+    } else if(numberInHousehold === 4){     // user lives with 3 other people
+        return 8;
+    } else if(numberInHousehold === 5){     // user lives with 4 other people
+        return 6;
+    } else if(numberInHousehold === 6){     // user lives with 5 other people
+        return 4;
+    } else if(numberInHousehold > 6){       // user lives with more thawn 5 other people
+        return 2;
+    }
+    console.log(`Invalid number of people in household given: ${inputText}`);
+    return 0;
+}
+
+function determineHouseSizePts(val = null){
+    if(val){
+        if(val === "large"){
+            return 10;
+        }else if(val === "medium"){
+            return 7;
+        }else if(val === "small"){
+            return 4;
+        }else if(val === "apartment"){
+            return 2;
+        }
+    }
+    return 0;   // invalid size, return zero
+}
+
+export {determineHouseSizePts, determineHouseholdPts};
---
+++ w9/main.js
@@ -0,0 +1,66 @@
+import { renderTbl } from "./render.js";
+import { determineHouseSizePts, determineHouseholdPts } from "./footprint.js";
+import { FORM, cfpDataList } from "./global.js";
+
+const FIRSTNAME = document.getElementById("firstname");
+const LASTNAME = document.getElementById("lastname");
+const HOUSEMEMBERS = document.getElementById("housemembers");
+
+// add a record to the data list
+function addCFRecord( firstname, lastname, householdMembers, houseSize ){
+    const cfpData = {
+        firstName:          firstname,
+        lastName:           lastname,
+        householdMembers:   householdMembers,
+        houseSize:          houseSize,
+        houseHoldPts:       determineHouseholdPts(householdMembers),
+        houseSizePts:       determineHouseSizePts(houseSize),
+        total:              undefined,
+        calcTotal: function(){
+            this.total = this.houseHoldPts + this.houseSizePts;
+        }
+    };
+    cfpData.calcTotal();    // calculate the totals after the object is instantiated
+
+    cfpDataList.push(cfpData);  // push the object to the array
+}
+
+// We have our validateFieldInput() function work on a given field so that we
+// can use it for both the focus loss event handlers, and the form submission
+// handler.
+
+// ensure given field is valid, return false if not
+function validateFieldInput(field){
+    const errMsg = document.getElementById("error_message");    // div for error message output
+    if(field.value === ""){
+        // intput is invalid, output the error and add invalid class
+        errMsg.textContent = `ERROR: Field "${field.id}" is required.`;
+        field.classList.add("invalid");
+        return false;
+    }
+    // intput is valid, clear the error message and invalid class
+    errMsg.textContent = "";
+    field.classList.remove("invalid");
+    return true;
+}
+
+// on firstname or lastname focus loss
+FIRSTNAME.addEventListener("blur", (e) => validateFieldInput(e.target));
+LASTNAME.addEventListener("blur", (e) => validateFieldInput(e.target));
+HOUSEMEMBERS.addEventListener("blur", (e) => validateFieldInput(e.target));
+
+// on form submission
+FORM.addEventListener("submit", function(e){
+    e.preventDefault();
+    const firstName = FORM.firstname.value;
+    const lastName = FORM.lastname.value;
+    const houseMembers = parseInt(FORM.housemembers.value);
+    const houseSize = FORM.housesize.value;
+
+    // final check for valid field inputs
+    if(validateFieldInput(FIRSTNAME) && validateFieldInput(LASTNAME) && validateFieldInput(HOUSEMEMBERS)){
+        addCFRecord(firstName, lastName, houseMembers, houseSize);
+        renderTbl(cfpDataList);
+        FORM.reset();
+    }
+});
